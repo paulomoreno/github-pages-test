fde25a2f436a7ff9d6582784066a58541b43c38c
Paulo Moreno <paulomorenoo@gmail.com>
2019-06-01 09:54:05 -0300

    Updates

---
+++ precache-manifest.e3c089af249853474eb81e153bf9e3da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074662ef5ace8506acaebdc6e19a7183",
+    "url": "/github-pages-test/index.html"
+  },
+  {
+    "revision": "93ebb305a01273554086",
+    "url": "/github-pages-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/github-pages-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "93ebb305a01273554086",
+    "url": "/github-pages-test/static/js/main.fbae3903.chunk.js"
+  },
+  {
+    "revision": "f4c95823f421432a6441",
+    "url": "/github-pages-test/static/js/runtime~main.aa321065.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
